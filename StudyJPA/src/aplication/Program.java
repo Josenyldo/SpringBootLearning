54c472d94755c804a80156b61839025b6f4040b9
josenyldo <josenildo.simao@gmail.com>
2021-01-08 00:26:36 -0300

    Funcionando
    Salva dados em um banco de dados existente e também recupera esses dados.

---
+++ StudyJPA/src/aplication/Program.java
@@ -0,0 +1,15 @@
+package aplication;
+import domain.Parson;
+
+public class Program {
+	public static void main(String arg[]) {
+		
+		Parson p1 = new Parson(1,"Maycon","josenildo.gmail.com");
+		Parson p2 = new Parson(2,"Vieira","asdasd.gmail.com");
+		Parson p3 = new Parson(3,"Pedro","jofdsfsd.gmail.com");
+		System.out.println(p1);
+		System.out.println(p2);
+		System.out.println(p3);
+	}
+
+}
